c68948eaa51befc313000354c1d14e93414afefa
AuraComex <auracomex@outlook.com>
2026-05-22 01:30:55 -0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 5173,
+    strictPort: false
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false
   }
 })
